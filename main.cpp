0cee952a517cca97740bc7f95c7cfd83e27d2457
Thai Hoa <pth3141@gmail.com>
2026-06-11 21:39:02 +0700

    Cap nhat thu muc

--- main.cpp
+++ main.cpp
@@ -6,10 +6,47 @@
 #include <vector>
 #include <sstream>
 #include <csignal>
-#include "Feature/features.h"
+#include "Feature/feature.h"
 
 using namespace std;    
 
-int main(){
+void print_startup_info() {
+    DWORD pid = GetCurrentProcessId();
+    cout << "========================================" << endl;
+    cout << "              Small Shell                " << endl;
+    cout << "========================================" << endl;
+    cout << "Welcome to Small Shell!" << endl;
+    cout << "This is a simple shell program to interact with the Windows operating system." << endl;
+    cout << "PID of Small Shell: " << pid << endl;
+    cout << "Type 'help' to see the list of available commands." << endl;
+    cout << "========================================" << endl;
+}
 
+void execute_command(const string& command, const vector<string>& args){
+
+}
+
+vector<string> split_input(const string& input){
+    vector<string> tokens;
+    stringstream ss(input);
+    string token;
+
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    // Giải quyết alias cho lệnh đầu tiên
+    if (!tokens.empty()) {
+        const string& first_token = tokens[0];
+        if (first_token == "cls") {
+            tokens[0] = "clear";
+        }
+    }
+
+    return tokens;
+}
+
+int main(){
+    print_startup_info();
+    return 0;
 }
